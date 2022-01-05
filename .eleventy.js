5b7c662b4e6c826893dde9b4f0eaa7520707a0c5
Noevenzahn <info.artuofficial@gmail.com>
2022-01-05 11:06:31 +0100

    add favicon and minor SCSS changes

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfiq) {
 
     eleventyConfiq.addPassthroughCopy("./src/assets")
     eleventyConfiq.addPassthroughCopy("./src/admin")
+    eleventyConfiq.addPassthroughCopy("favicon.ico")
 
     return {
         dir: {
